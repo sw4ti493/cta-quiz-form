5db948feeec1eac7a84175dd0bf24dd3babe83fe
Swati Kumar <kumar.swati16@gmail.com>
2015-01-30 11:10:24 -0800

    delete error messages

--- send_form_email.php
+++ send_form_email.php
@@ -70,35 +70,7 @@ if(isset($_POST['email'])) {
  
      
  
-    $error_message = "";
- 
-    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
- 
-  if(!preg_match($email_exp,$email_from)) {
- 
-    $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
- 
-  }
- 
-    $string_exp = "/^[A-Za-z .'-]+$/";
- 
-  if(!preg_match($string_exp,$question_1_A)) {
- 
-    $error_message .= 'The answer you entered does not appear to be valid.<br />';
- 
-  }
- 
-  if(!preg_match($string_exp,$question_1_B)) {
- 
-    $error_message .= 'The answer you entered does not appear to be valid.<br />';
- 
-  }
- 
-  if(strlen($question_1_C) < 2) {
- 
-    $error_message .= 'The answer you entered do not appear to be valid.<br />';
- 
-  }
+    
  
   if(strlen($error_message) > 0) {
  
