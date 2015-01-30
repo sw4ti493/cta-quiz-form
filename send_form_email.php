6a1fc6cd9c71328d8b0a8941821f010b9c7fcbbd
Swati Kumar <kumar.swati16@gmail.com>
2015-01-30 13:34:44 -0800

    new form

--- send_form_email.php
+++ send_form_email.php
@@ -1,63 +1,35 @@
 <?php
- 
-if(isset($_POST['email'])) {
- 
-     
- 
-    // EDIT THE 2 LINES BELOW AS REQUIRED
- 
-    $email_to = "skumar@cta.org";
- 
-    $email_subject = "New CTA Quiz Submission";
- 
-     
- 
-     
- 
-    function died($error) {
- 
-        // your error code can go here
- 
-        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
- 
-        echo "These errors appear below.<br /><br />";
- 
-        echo $error."<br /><br />";
- 
-        echo "Please go back and fix these errors.<br /><br />";
- 
-        die();
- 
-    }
- 
-     
- 
-   
- 
-// create email headers
- 
-$headers = 'From: '.$email_from."\r\n".
- 
-'Reply-To: '.$email_from."\r\n" .
- 
-'X-Mailer: PHP/' . phpversion();
- 
-@mail($email_to, $email_subject, $email_message, $headers);  
- 
+  $question_1_A = $_POST['question_1_A'];
+  $question_1_B = $_POST['question_1_B'];
+  $question_1_C = $_POST['question_1_C'];
+  $question_1_D = $_POST['question_1_D'];
+  $question_1_E = $_POST['question_1_E'];
+  $question_1_F = $_POST['question_1_F'];
+  $question_1_G = $_POST['question_1_G'];
 ?>
+
+<?php
+    $email_from = 'New Quiz User';
  
+    $email_subject = "New Quiz submission";
  
- 
-<!-- include your own success html here -->
- 
- 
- 
-Thank you for submitting your quiz.
- 
- 
- 
+    $email_body = "API: $question_1_A.\n".
+                            "PSAA: $question_1_B".\n
+                            "ALD: $question_1_C".\n
+                            "CAASPP: $question_1_D".\n
+                            "SBAC: $question_1_E".\n
+                            "CCSS: $question_1_F".\n
+                            "ICA: $question_1_G".\n
+?>
+
 <?php
  
-}
+  $to = "skumar@cta.org";
  
-?>
+  $headers = "From: $email_from \r\n";
+ 
+  $headers .= "Reply-To: $visitor_email \r\n";
+ 
+  mail($to,$email_subject,$email_body,$headers);
+ 
+ ?>
